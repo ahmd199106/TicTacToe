91cf26a985d8f5642c28f130c8fcf22f089b68d1
Ahmed <ahmd199106@gmail.com>
2020-10-24 03:05:50 +0530

    TicTacToe

---
+++ src/components/Game.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Board from './Board';
+
+const Game = () => {
+
+    const handleClick = () => {
+        
+    }
+    
+    const jumpTo = () => {
+        
+    }
+    
+    const renderMoves = () => {
+        
+    }
+
+
+
+return (
+    <Board onClick={handleClick} />
+
+)
+}
+export default Game;
